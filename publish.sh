e5dcf439f2b85ccd4ab9bee71995e756adcd9bbb
Jacob Matuskey <jmatuskey@stsci.edu>
2019-01-28 13:28:28 -0500

    modify publish to make sure the script stops if you need to pull

--- publish.sh
+++ publish.sh
@@ -18,13 +18,17 @@ set -xe
 # reverse pipe so vars are remembered in main shell
 read repo major minor patch extra <<< $(sed 's/\./ /g' ./${folder}/VERSION)
 
-docker login
 $(aws ecr get-login --no-include-email --region us-east-1)
 
 docker build -t ${hub}/${repo}:latest -t ${hub}/${repo}:latest -t ${hub}/${repo}:${major} -t ${hub}/${repo}:${major}.${minor} -t ${hub}/${repo}:${major}.${minor}.${patch} ./${folder}
 
+# make sure your local branch isn't stale
+git fetch
+$(git rev-parse HEAD) == $(git rev-parse @{u})
+
 # patch++ and tag repo.
-tag=`cat ./${folder}/VERSION`
+temptag=`cat ./${folder}/VERSION`
+tag=${temptag%.devel}
 
 awk -F. '{ OFS=FS; $4++ } 1' ./${folder}/VERSION > tmp
 mv tmp ./${folder}/VERSION
